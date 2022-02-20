be8919e492bc4c676742b6b981bd8a904247b6a0
MrMillerX <1marcusmiller1@gmail.com>
2022-02-20 19:13:03 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,94 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+//    @GetMapping()
+//    public String getAllUsers(Model model) {
+//        model.addAttribute("users", userService.getAllUsers());
+//        return "users/index";
+//    }
+
+    @GetMapping()
+    public String getAllUsers(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("thisUser", user);
+        model.addAttribute("newUser", new User());
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "admin";
+    }
+
+    @PostMapping("/new")
+    public String createUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "rol") ArrayList<Long> longs) {
+        Set<Role> bufSet = new HashSet<>();
+        for (long l : longs) {
+            bufSet.add(roleService.getRole(l));
+        }
+        user.setRoles(bufSet);
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+//    @GetMapping("/new")
+//    public String newUser(Model model) {
+//        model.addAttribute("thisUser", new User());
+//        model.addAttribute("roles", roleService.getAllRoles());
+//        return "users/new";
+//    }
+
+    @PatchMapping("/edit")
+    public String updateUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "rol") ArrayList<Long> longs) {
+        Set<Role> bufSet = new HashSet<>();
+        for (long l : longs) {
+            bufSet.add(roleService.getRole(l));
+        }
+        user.setRoles(bufSet);
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+
+//    @DeleteMapping("/{id}")
+//    public String deleteUser(@PathVariable("id") long id) {
+//        userService.deleteUser(id);
+//        return "redirect:/admin";
+//    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+//    @GetMapping("/{id}/edit")
+//    public String editUser(Model model, @PathVariable("id") long id) {
+//        model.addAttribute("thisUser", userService.getUser(id));
+//        model.addAttribute("roles", roleService.getAllRoles());
+//        return "users/edit";
+//    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller
+@RequestMapping()
+public class UserController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/login";
+    }
+
+    @GetMapping("/user")
+    public String showUserInfo(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+}
